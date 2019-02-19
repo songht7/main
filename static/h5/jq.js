9847e932707074511e35ab5156a445c19895d852
leon ting <leon.ting@okkuaixiu.com>
2019-02-19 17:36:31 +0800

    add ef

--- static/h5/jq.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$(document).on("click", "#GoTo", function() {
-		var objHeight = $("#ApplyFormBox").offset().top;
-		$('html,body').animate({
-			scrollTop: objHeight
-		}, 800);
-	});
-});
