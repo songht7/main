36cc004d59577e88b6e4a821378ac3c7afa7bc70
leon ting <leon.ting@okkuaixiu.com>
2019-02-28 14:49:31 +0800

    #add weibo page

--- static/h5/base-h5.js
+++ static/h5/base-h5.js
@@ -12,12 +12,12 @@ $(function() {
 					$(".GoTo").hide();
 				}
 			} else {
-				var g = Math.round(dcHeight) - Math.round(objOffTop) - 120;
-				if (g <= Math.round(_top)) {
-					$(".GoTo").hide();
-				} else {
-					$(".GoTo").show();
-				}
+// 				var g = Math.round(dcHeight) - Math.round(objOffTop) - 120;
+// 				if (g <= Math.round(_top)) {
+// 					$(".GoTo").hide();
+// 				} else {
+// 					$(".GoTo").show();
+// 				}
 			}
 
 		}
